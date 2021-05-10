69c3557fb79c4a318be9ef905453ab82aee8503c
Andrew Zuniga <azuniga183@gmail.com>
2021-05-10 16:32:26 -0700

    updated salaries according to google and not example given

--- seeds.sql
+++ seeds.sql
@@ -10,19 +10,19 @@ VALUE ("Legal");
 
 -- role seeds
 INSERT INTO role (title, salary, department_id)
-VALUE ("Lead Engineer", 150000, 2);
+VALUE ("Lead Engineer", 130000, 2);
 INSERT INTO role (title, salary, department_id)
-VALUE ("Legal Team Lead", 250000, 4);
+VALUE ("Legal Team Lead", 143000, 4);
 INSERT INTO role (title, salary, department_id)
-VALUE ("Sales Lead", 100000, 1);    
+VALUE ("Sales Lead", 30000, 1);    
 INSERT INTO role (title, salary, department_id)
-VALUE ("Accountant", 125000, 3);
+VALUE ("Accountant", 70000, 3);
 INSERT INTO role (title, salary, department_id)
-VALUE ("Salesperson", 80000, 1);
+VALUE ("Salesperson", 50000, 1);
 INSERT INTO role (title, salary, department_id)
-VALUE ("Software Engineer", 120000, 2);
+VALUE ("Software Engineer", 135000, 2);
 INSERT INTO role (title, salary, department_id)
-VALUE ("Lawyer", 190000, 4);
+VALUE ("Lawyer", 153000, 4);
 
 -- employee seeds
 INSERT INTO employee (first_name, last_name, manager_id, role_id)
